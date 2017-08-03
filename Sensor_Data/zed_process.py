85633c37091bdab7da4f909aa8eef85b680e43b7
Arda Mavi <ardamavi2@gmail.com>
2017-08-03 16:27:45 +0300

    Added camera depth calculations

--- Sensor_Data/zed_process.py
+++ Sensor_Data/zed_process.py
@@ -10,16 +10,18 @@ def get_capture(camera=1): # Camera 0 is jetson's embeded camera.
 def release_capture(capture):
     capture.release()
 
+stereo = cv2.StereoBM_create(numDisparities=16, blockSize=15)
+
 # Take a picture:
 def get_zed_data(capture):
     while True:
         ret, img = capture.read()
-        img_left = img[0:720, 0:1280]
-        img_right = img[0:720, 1280:2560]
 
-        # TODO: Stereo picture
-        # stereo = cv2.StereoBM_create(numDisparities=16, blockSize=15)
-        # img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        # disparity = stereo.compute(img_left,img_right)
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
-    return img
+        img_left = img[0:376, 0:672]
+        img_right = img[0:376, 672:1344]
+
+        disparity = stereo.compute(img_left,img_right)
+
+    return disparity
