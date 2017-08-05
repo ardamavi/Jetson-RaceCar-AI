08170d025e26beb45dfdae7bbb7a6bfbe7d0db1d
Arda Mavi <ardamavi2@gmail.com>
2017-08-05 13:55:05 +0300

    Used multiprocessing library

--- ai.py
+++ ai.py
@@ -2,24 +2,22 @@
 
 import numpy as np
 from scipy.misc import imresize
-from threading import Thread
+from multiprocessing import Process, Value, Array
 from predict import predict, get_ready_model
 from Sensor_Data.zed_process import get_zed_data, get_capture, release_capture
 from Sensor_Data.lidar_process import get_lidar_data, start_lidar, stop_lidar
 
-zed_data = np.array([])
-lidar_data = np.array([])
-data_flow = False
+zed_data = Array('f', [])
+lidar_data = Array('f', [])
+data_flow = Value('i', 1)
 
 def zed_data_process(cap):
-    while True:
-        if data_flow:
-            zed_data = np.array(imresize(np.array(get_zed_data(cap)), (500, 500, 1)))
+    while data_flow.value:
+        zed_data = np.array(imresize(np.array(get_zed_data(cap)), (500, 500, 1)))
 
 def lidar_data_process():
-    while True:
-        if data_flow:
-            lidar_data = np.array(get_lidar_data())
+    while data_flow.value:
+        lidar_data = np.array(get_lidar_data())
 
 def ai():
     print('Preparing model ...')
@@ -34,30 +32,31 @@ def ai():
     cap = get_capture()
     print('Camera ready.')
 
-    data_flow = True
+    data_flow.value = True
 
     print('Threads starting')
     # Start getting data process:
-    camera_process = Thread(target=zed_data_process, args=(cap,))
+    camera_process = Process(target=zed_data_process, args=(cap,))
     camera_process.start()
     print('Camera thread start.')
 
-    lidar_process = Thread(target=lidar_data_process)
+    lidar_process = Process(target=lidar_data_process)
     lidar_process.start()
     print('Lidar thread start.')
 
     print('AI will start in a short time.')
 
     while True:
-        while zed_data.size == 0 or lidar_data.size == 0:
-            pass
+        stereo_img = np.array(zed_data)
+        lidar_map = np.array(lidar_data)
         try:
-            print(predict(model, zed_data, lidar_data))
+            if stereo_img.size == 1 and lidar_map.size == 1:
+                print(predict(model, stereo_img, lidar_map))
         except:
             print('AI Error !')
             break
 
-    data_flow = False
+    data_flow.value = False
     camera_process.join()
     lidar_process.join()
 
